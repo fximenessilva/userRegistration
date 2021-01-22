f810625e8bba0b2e724322a5d916a3231e94400d
francisco ximenes <fximenessilva@gmail.com>
2021-01-22 13:39:51 +0100

    added logic

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,3 +1,8 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable react/sort-comp */
+/* eslint-disable react/no-deprecated */
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prefer-stateless-function */
@@ -20,12 +25,20 @@ const initialState = {
 };
 
 export default class UserCrud extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...initialState };
+  }
 
-  state = {...initialState}
+  componentWillMount() {
+    axios(baseUrl).then((resp) => {
+      this.setState({ list: resp.data });
+    });
+  }
 
-  getUpdatedList(user) {
+  getUpdatedList(user, add = true) {
     const list = this.state.list.filter((u) => u.id !== user.id);
-    list.unshift(user);
+    if (add) list.unshift(user);
     return list;
   }
 
@@ -46,13 +59,127 @@ export default class UserCrud extends Component {
 
   updateField(event) {
     const user = { ...this.state.user };
-    user[event.target.name]
+    user[event.target.name] = event.target.value;
+    this.setState({ user });
+  }
+
+  renderForm() {
+    return (
+      <div className="form">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label htmlFor="name">Nome</label>
+              <input
+                type="text"
+                id="name"
+                className="form-control"
+                name="name"
+                value={this.state.user.name}
+                onChange={(e) => this.updateField(e)}
+                placeholder="Digite o nome..."
+              />
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label htmlFor="email">E-mail</label>
+
+              <input
+                type="text"
+                id="email"
+                className="form-control"
+                name="email"
+                value={this.state.user.email}
+                onChange={(e) => this.updateField(e)}
+                placeholder="Digite o email..."
+              />
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col-12 d-flex justify-content-end">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={(e) => this.save(e)}
+            >
+              Salvar
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary ml-2"
+              onClick={(e) => this.clear(e)}
+            >
+              Cancelar
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  load(user) {
+    this.setState({ user });
+  }
+
+  remove(user) {
+    axios.delete(`${baseUrl}/${user.id}`).then((resp) => {
+      const list = this.getUpdatedList(user, false);
+      this.setState({ list });
+    });
+  }
+
+  renderTable() {
+    return (
+      <table className="table mt-4">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderRows()}
+        </tbody>
+      </table>
+    );
+  }
+
+  renderRows() {
+    return this.state.list.map((user) => (
+      <tr key={user.id}>
+        <td>{user.id}</td>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>
+          <button
+            type="button"
+            className="btn btn-warning"
+            onClick={() => this.load(user)}
+          >
+            <i className="fa fa-pencil" />
+          </button>
+          <button
+            type="button"
+            className="btn btn-danger ml-2"
+            onClick={() => this.remove(user)}
+          >
+            <i className="fa fa-trash" />
+          </button>
+        </td>
+      </tr>
+    ));
   }
 
   render() {
     return (
       <Main {...headerProps}>
-        Cadastro de Usuário
+        {this.renderForm()}
+        {this.renderTable()}
       </Main>
     );
   }
